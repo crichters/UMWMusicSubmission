80ea2e6792c973a9a8526e18de04509688eb56ff
s-sandra <aleksandra.shtabnaya@gmail.com>
2020-03-22 21:05:49 -0400

    Add schema creation scripts and some node tests

---
+++ rsms_insert.sql
@@ -0,0 +1,161 @@
+insert into recital (date, start_time, end_time) values ("2020-04-06", "19:30:00", "20:30:00");
+insert into recital (date, start_time, end_time) values ("2020-04-23", "16:00:00", "17:00:00");
+
+insert into submission (
+	duration, 
+	title, 
+	larger_work, 
+	email, 
+	composer_name, 
+	composer_birth_year, 
+	composer_death_year, 
+	catalog_num, 
+	scheduling_req, 
+	tech_req,
+	movement,
+	recital_id
+)
+values (
+	8,
+	"Adagio and Allegro",
+	"Sonata",
+	"jcook@umw.edu",
+	"G.F. Handel",
+	1685,
+	1759,
+	"No. 1",
+	null,
+	"Need a music stand",
+	null,
+	1 -- id is the id associated with form's date drop-down menu item
+);
+
+insert into recital_submissions (
+	recital_id,
+	submission_id
+)
+values (
+	1,
+	(select last_insert_id())  -- get the id of the submission we just inserted
+);
+
+insert into performer (name, medium) values ("Jordan Cooke", "saxophone");
+insert into performer (name, medium) values ("Cathy Hopkins", "piano");
+
+insert into submission_performers (
+	submission_id,
+	performer_id
+)
+values (
+	(select id from submission order by id desc limit 1),
+	(select id from performer where name = "Jordan Cooke" and medium = "saxophone")
+);
+
+insert into submission_performers (
+	submission_id,
+	performer_id,
+	is_collaborator
+)
+values (
+	(select id from submission order by id desc limit 1),
+	(select id from performer where name = "Cathy Hopkins" and medium = "piano"),
+	true
+);
+
+insert into submission (
+	duration, 
+	title, 
+	larger_work, 
+	email, 
+	composer_name, 
+	composer_birth_year, 
+	composer_death_year, 
+	catalog_num, 
+	scheduling_req, 
+	tech_req,
+	movement,
+	recital_id
+)
+values (
+	3,
+	"Here Comes the Sun",
+	null,
+	"clyman@umw.edu",
+	"George Harrison",
+	1943,
+	2001,
+	null,
+	"I can't perform after 4:00 because I have soccer.",
+	null,
+	null,
+	1 -- id is the id associated with form's date drop-down menu item
+);
+
+insert into recital_submissions (
+	recital_id,
+	submission_id
+)
+values (
+	1,
+	(select last_insert_id())  -- get the id of the submission we just inserted
+);
+
+insert into performer (name, medium) values ("Christian Lyman", "guitar");
+
+insert into submission_performers (
+	submission_id,
+	performer_id
+)
+values (
+	(select id from submission order by id desc limit 1),
+	(select id from performer where name = "Christian Lyman" and medium = "guitar")
+);
+
+insert into submission (
+	duration, 
+	title, 
+	larger_work, 
+	email, 
+	composer_name, 
+	composer_birth_year, 
+	composer_death_year, 
+	catalog_num, 
+	scheduling_req, 
+	tech_req,
+	movement,
+	recital_id
+)
+values (
+	1,
+	"March",
+	"Notebook for Anna Magdalena Bach",
+	"jshin2@umw.edu",
+	"Johann Sebastian Bach",
+	1685,
+	1750,
+	null,
+	null,
+	null,
+	null,
+	2 -- id is the id associated with form's date drop-down menu item
+);
+
+insert into recital_submissions (
+	recital_id,
+	submission_id
+)
+values (
+	2,
+	(select last_insert_id())  -- get the id of the submission we just inserted
+);
+
+insert into performer (name, medium) values ("Julie Shin", "piano");
+
+insert into submission_performers (
+	submission_id,
+	performer_id
+)
+values (
+	(select id from submission order by id desc limit 1),
+	(select id from performer where name = "Julie Shin" and medium = "piano")
+);
---
+++ rsmsdb.js
@@ -0,0 +1,140 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('mysql://sandra:sandra@localhost:3306/alexshta_rsms');
+
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+
+insertRecital({date:"2020-08-01", start_time:"14:00:00", end_time:"15:00:00"});
+
+/**
+ * Returns a list of active recital objects.
+ * Objects contain the recital id (int), date (string), 
+ * endTime (string) and startTime (string).
+ */
+function selectOpenRecitals() {
+    db.query("SELECT id, " + 
+                    "DATE_FORMAT(date, '%M %e, %Y') as date, " + 
+                    "TIME_FORMAT(start_time, '%h:%i %p') as startTime, " +
+                    "TIME_FORMAT(end_time, '%h:%i %p') as endTime " + 
+            "FROM recital WHERE NOT is_closed"
+    , { type: db.QueryTypes.SELECT})
+  .then(recitals => {
+      console.log("Open recitals:", JSON.stringify(recitals, null, 4))
+   });
+};
+
+/**
+ * Returns a list of recital objects that
+ * appear in the dashboard. Objects contain
+ * the recital id (int), date (string), endTime (string), 
+ * startTime (string) and isClosed status (boolean).
+ */
+function selectUnarchivedRecitals() {
+    db.query("SELECT id, " + 
+                    "DATE_FORMAT(date, '%M %e, %Y') as date, " + 
+                    "TIME_FORMAT(start_time, '%h:%i %p') as startTime, " +
+                    "TIME_FORMAT(end_time, '%h:%i %p') as endTime, " +
+                    "is_closed as isClosed " +
+            "FROM recital WHERE NOT is_archived"
+    , { type: db.QueryTypes.SELECT})
+  .then(recitals => {
+    console.log("Dashboard recitals:", JSON.stringify(recitals, null, 4))
+  });
+};
+
+/**
+ * Returns list of submission objects for a given
+ * recital. Objects contain submission id, performer name,
+ * medium, title, largerWork (null if not specified) and status.
+ * @param {int} recitalId - The recital's pk. 
+ */
+function selectSubmissionsFor(recitalId) {
+    db.query("SELECT sub.id, " +
+                "sub.larger_work AS largerWork, " +
+                "sub.title, " +
+                "performer.name, " +
+                "performer.medium " + 
+            "FROM submission AS sub " +
+            "NATURAL JOIN recital_submissions " +
+            "INNER JOIN submission_performers AS performers " +
+                "ON sub.id = performers.submission_id " +
+            "INNER JOIN performer " +
+                "ON performers.performer_id = performer.id " +
+            "WHERE NOT performers.is_collaborator " +
+            `AND recital_id = ${recitalId}`
+    , { type: db.QueryTypes.SELECT})
+  .then(submissions => {
+    console.log("Submissions:", JSON.stringify(submissions, null, 4));
+  });
+}
+
+/**
+ * Returns submission object for submission of given
+ * id. Object contains all attributes of submission,
+ * including fields that were left blank.
+ * @param {int} submissionId - The submission's pk.
+ */
+function selectSubmissionDetailsFor(submissionId) {
+    db.query("SELECT sub.duration, " +
+                    "sub.title, " +
+                    "sub.larger_work, " +
+                    "sub.email, " +
+                    "sub.composer_name, " +
+                    "sub.composer_birth_year, " +
+                    "sub.composer_death_year, " +
+                    "sub.catalog_num," +
+                    "sub.scheduling_req, " +
+                    "sub.tech_req, " +
+                    "sub.movement, " +
+                    "recital.date, " +
+                    "perf.name," +
+                    "perf.medium " +
+            "FROM submission AS sub " +
+            "NATURAL JOIN recital " +
+            "NATURAL JOIN submission_performers AS perfs " +
+            "INNER JOIN performer AS perf " +
+                "ON perfs.performer_id = perf.id " +
+            "WHERE NOT perfs.is_collaborator " +
+            `AND sub.id = ${submissionId}`
+    , { type: db.QueryTypes.SELECT})
+
+  .then(submissions => {
+    submissions.map(submission => {
+        db.query("SELECT perf.name, perf.medium FROM submission_performers AS perfs " + 
+                 "INNER JOIN performer AS perf " +
+                    "ON perfs.performer_id = perf.id " +
+                `WHERE perfs.submission_id = ${submissionId} ` +
+                "AND perfs.is_collaborator", {type: db.QueryTypes.SELECT})
+        .then(collaborators => {
+            submission.collaborators = collaborators;
+            console.log("Submissions:", JSON.stringify(submissions, null, 4));
+        });
+    });
+  });
+}
+
+/**
+ * Insert submission into rsms db for a given recital.
+ * @param {Object} submission - The submission details. 
+ * @param {Object[]} collaborators - A list of collaborator objects.
+ * @param {int} recitalId - The recital's pk.
+ */
+function insertSubmission(submission, collaborators, recitalId) {
+};
+
+/*
+* Insert passed recital object into rsms db. Recital
+* objected should contain date (YYYY-MM-DD), end_time
+* (HH:mm:ss) and start_time (HH:mm:ss).
+*/
+function insertRecital(recital) {
+};
+
+module.exports = {selectOpenRecitals, selectSubmissionDetailsFor, selectSubmissionsFor,
+        selectUnarchivedRecitals, insertRecital, insertSubmission};
