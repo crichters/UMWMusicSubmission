979b1de35999f9da992bb1f8b32414b8af45216d
Simeon Neisler <simeon.neisler@gmail.com>
2020-04-02 17:25:12 -0400

    worked on captcha

--- index.js
+++ index.js
@@ -5,19 +5,46 @@ const { selectOpenRecitals, selectSubmissionDetailsFor, selectSubmissionsFor,
 
 const app = express();
 
+const {keys} = require('./config/config');
+
 app.set("port", 3000);
 
 app.use(express.static('content'));
 app.use(bodyParser.json({type: "application/json"}));
 app.use(bodyParser.urlencoded({extended: true}));
 
+const directory = __dirname + '/content';
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/content/template.html');
+    res.sendFile(directory + '/template.html');
 });
 
 //This is a get request that simply returns the login page
 app.get("/login-page", (req, res) => {
-    res.sendFile(__dirname + '/content/login_page.html');
+    res.sendFile(directory + '/login_page.html');
+});
+
+app.get("/dashboard", (req, res) => {
+    res.sendFile(directory + '/dashboard.html')
+});
+
+app.get("/dashboard-data", async (req, res) => {
+    let recitals = await selectOpenRecitals();
+    let promises = [];
+    recitals.forEach((recital) => {
+        promises.push(selectSubmissionsFor(recital.id));
+    });
+    let submissions = await Promise.all(promises);
+    let i = 0;
+    console.log(submissions);
+    recitals = recitals.map((recital) => {
+        return {
+            ...recital,
+            submissions: submissions[i]
+        }
+    });
+    console.log(recitals);
+    res.json(recitals);
 });
 
 //This get request is used to actually sign the user in.
@@ -31,17 +58,59 @@ app.get("/form", (req, res) => {
 
 app.get("/get-recitals", async (req, res) => {
     const recitals = await selectOpenRecitals();
-    console.log(recitals);
     res.json(recitals);
 
 });
 
 app.post("/submit_recital_form", async (req, res) => {
-    const submission = {};
-    const response = await insertSubmission(submission)
+    if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null)
+    {
+      return res.json({"responseError" : "Please select captcha first"});
+    }
+  
+    const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + keys.captchaPrivate + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+  
+    request(verificationURL, (error,response,body) => {
+      body = JSON.parse(body);
+  
+      if(body.success !== undefined && !body.success) {
+        return res.json({"responseError" : "Failed captcha verification"});
+      }
+    });
+    let {name, medium, duration, selection_title, selection_work, catalog_number, movement, email, composer_name, composer_birth, composer_death, schedule_requirements, technical_requirements, collaborators, recital_date} = req.body;
+    console.log(req.body);
+    collaborators = collaborators.map((c) => {
+        return {
+            name: c.collaborator_name,
+            medium: c.collaborator_medium
+        }
+    });
+    const performer = {
+        name,
+        medium
+    }
+    const submission = {
+        duration: duration,
+        title: selection_title,
+        largerWork: selection_work,
+        email, 
+        composerName: composer_name,
+        composerBirthYear: composer_birth, 
+        composerDeathYear: composer_death,
+        catalogNum: catalog_number,
+        schedulingReq: schedule_requirements,
+        techReq: technical_requirements,
+        movement
+    };
+    const response = await insertSubmission(submission, performer, collaborators, recital_date);
+    res.sendFile(directory +'/submitted.html');
 });
 
 app.get("/create-recital", (req, res) => {
+
+});
+
+app.get("/get-submissions", async (req, res) => {
 
 });
 
