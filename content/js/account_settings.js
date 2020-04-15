f1a6c713c94182c8f61805eb600e3c1b92d956c6
crichters <cgrichters@gmail.com>
2020-04-15 18:19:22 -0400

    Added password and email functionality for account-settings

--- content/js/account_settings.js
+++ content/js/account_settings.js
@@ -4,12 +4,70 @@ $('#email_form').submit((e) => {
     
     var email_address = $('#new_email').val();
 
+    $.post("/add_email", email_address, (data, success) => {
+        console.log(data);
+        console.log(success);
+    });
     
-    
-})
+});
+
+function clear_passwords() {
+    $('#old_password').val('');
+    $('#new_password').val('');
+    $('#new_password2').val('');
+}
+
+$('#password_form').submit((e) => {
+    e.preventDefault();
+
+    var current_password = $('#old_password').val();
+    var new_password = $('#new_password').val();
+    var new_password2 = $('#new_password2').val();
+
+    if (new_password === new_password2){
+        change_request = {
+            "current_password": current_password,
+            "new_password": new_password
+        };
+        
+        $.post("/change-password", change_request, (data, success) => {
+            clear_passwords();
+            if(data === true) {
+                $('#response').html(`<span style="color:green;">Password changed successfully</span>`);
+            } else {
+                $('#response').html(`<span style="color:red;">Error changing password</span>`);
+            }
+        });
+    } else {
+        $('#response').html(`<span style="color:red;">Error: New Passwords do not match</span>`);
+    }
+});
+
+function test_emails() {
+    for(var i = 0; i < 5; i++) {
+        append_email(i);
+    }
+}
+
+function append_email(email_address) {
+    var table_row = `
+    <tr>
+      <td>${email_address}</td>
+      <td><a href="#" onclick="delete_email(${email_address})">Delete</a></td>
+    </tr>`;
+
+    $('#email_table').append(table_row);
+}
+
+function delete_email(email_address) {
+    console.log(email_address);
 
 
-
+    $.post("/delete_email", email_address, (data, status) => {
+        console.log(data);
+        console.log(success);
+    });
+}
 
 
 $(document).ready(function() {
@@ -19,4 +77,6 @@ $(document).ready(function() {
           console.log(data[i]);
         }
       });
+
+      test_emails();
     });
